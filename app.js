7a67c81ff6b2309cf8572a83260bd5b14d26ee32
mariacarolinass <m.carolina.soares1@gmail.com>
2025-10-13 18:41:19 -0300

    add middlewares ao app

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-import express, { json } from 'express';
+import express from 'express';
 import dotenv from "dotenv";
-
 import authRoutes from './src/routes/auth.routes.js';
 import userRoutes from './src/routes/users.routes.js';
 import tasksRoutes from './src/routes/tasks.routes.js';
+import LoggerMiddleware from "./src/middlewares/logger.middleware.js";
+import ErrorMiddleware from "./src/middlewares/error.middleware.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(LoggerMiddleware);
 
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/tasks', tasksRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/tasks', tasksRoutes);
+
+app.use(ErrorMiddleware);
 
 app.get('/', (req, res) => res.json({ ok: true }));
 
